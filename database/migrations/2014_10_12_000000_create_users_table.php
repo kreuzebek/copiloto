0baa2da7c1658d6c1a46910ee57a115c186c83d2
kreuzebek <kreuzebek@gmail.com>
2016-09-10 23:33:45 -0300

    new file:   .env.example 	new file:   .gitattributes 	new file:   .gitignore 	new file:   _ide_helper.php 	new file:   app/Console/Kernel.php 	new file:   app/Exceptions/Handler.php 	new file:   app/Http/Controllers/Auth/ForgotPasswordController.php 	new file:   app/Http/Controllers/Auth/LoginController.php 	new file:   app/Http/Controllers/Auth/RegisterController.php 	new file:   app/Http/Controllers/Auth/ResetPasswordController.php 	new file:   app/Http/Controllers/Controller.php 	new file:   app/Http/Kernel.php 	new file:   app/Http/Middleware/EncryptCookies.php 	new file:   app/Http/Middleware/RedirectIfAuthenticated.php 	new file:   app/Http/Middleware/VerifyCsrfToken.php 	new file:   app/Providers/AppServiceProvider.php 	new file:   app/Providers/AuthServiceProvider.php 	new file:   app/Providers/BroadcastServiceProvider.php 	new file:   app/Providers/EventServiceProvider.php 	new file:   app/Providers/RouteServiceProvider.php 	new file:   app/User.php 	new file:   artisan 	new file:   bootstrap/app.php 	new file:   bootstrap/autoload.php 	new file:   bootstrap/cache/.gitignore 	new file:   composer.json 	new file:   composer.lock 	new file:   config/app.php 	new file:   config/auth.php 	new file:   config/broadcasting.php 	new file:   config/cache.php 	new file:   config/compile.php 	new file:   config/database.php 	new file:   config/filesystems.php 	new file:   config/jwt.php 	new file:   config/mail.php 	new file:   config/queue.php 	new file:   config/services.php 	new file:   config/session.php 	new file:   config/view.php 	new file:   database/.gitignore 	new file:   database/factories/ModelFactory.php 	new file:   database/migrations/.gitkeep 	new file:   database/migrations/2014_10_12_000000_create_users_table.php 	new file:   database/migrations/2014_10_12_100000_create_password_resets_table.php 	new file:   database/seeds/.gitkeep 	new file:   database/seeds/DatabaseSeeder.php 	new file:   gulpfile.js 	new file:   package.json 	new file:   phpunit.xml 	new file:   public/.htaccess 	new file:   public/css/app.css 	new file:   public/favicon.ico 	new file:   public/index.php 	new file:   public/js/app.js 	new file:   public/robots.txt 	new file:   public/web.config 	new file:   readme.md 	new file:   resources/assets/js/app.js 	new file:   resources/assets/js/bootstrap.js 	new file:   resources/assets/js/components/Example.vue 	new file:   resources/assets/sass/app.scss 	new file:   resources/assets/sass/variables.scss 	new file:   resources/lang/en/auth.php 	new file:   resources/lang/en/pagination.php 	new file:   resources/lang/en/passwords.php 	new file:   resources/lang/en/validation.php 	new file:   resources/views/errors/503.blade.php 	new file:   resources/views/vendor/.gitkeep 	new file:   resources/views/welcome.blade.php 	new file:   routes/api.php 	new file:   routes/console.php 	new file:   routes/web.php 	new file:   server.php 	new file:   storage/app/.gitignore 	new file:   storage/app/public/.gitignore 	new file:   storage/framework/.gitignore 	new file:   storage/framework/cache/.gitignore 	new file:   storage/framework/sessions/.gitignore 	new file:   storage/framework/views/.gitignore 	new file:   storage/logs/.gitignore 	new file:   tests/ExampleTest.php 	new file:   tests/TestCase.php

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name',50);
+            $table->string('email',50)->unique();
+            $table->string('password',15);
+            $table->string('nickname',20)->nullable();
+            $table->string('phone',15)->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
