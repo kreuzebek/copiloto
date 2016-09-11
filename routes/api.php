d5dc0718f0c9385803cbe38cf8af5da81cb9a535
kreuzebek <kreuzebek@gmail.com>
2016-09-11 01:59:25 -0300

    Autenticacao adicionada

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,6 @@ use Illuminate\Http\Request;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:api');
+})->middleware('jwt.auth');
+
+Route::post('login', 'Api\AuthController@authenticate');
